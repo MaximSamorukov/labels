4f5c23c75e546af1d51d62e1fd2bdaa295f50f93
maksim.samorukov <maxim.samorukov@gmail.com>
2025-12-27 20:12:28 +0300

    wip

--- src/components/Controls/index.jsx
+++ src/components/Controls/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import html2canvas from 'html2canvas';
 import s from './style.module.scss';
 import { observer } from 'mobx-react-lite';
 import { useCustomContext } from '@/hooks/useCustomContext';
+import { state } from '@/state';
 
 export const Controls = observer(() => {
   const { addNewItem } = useCustomContext();
-
+  const previewItem = state.previewItem;
   const onAddItem = () => {
     addNewItem();
   };
-  const onSaveItem = () => {
+  const onSaveItem = async () => {
+    const el = document.getElementById('card');
+    const canvas = await html2canvas(el, { scale: 1 });
+    const link = document.createElement('a');
+    link.download = `card_${previewItem.id}.png`;
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+    console.log(el);
     //addNewItem();
   };
   return (
